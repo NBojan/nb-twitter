0f98b21b7e6db6bc1a1dd4cfb43d1233ae89a42b
NBojan <63514753+NBojan@users.noreply.github.com>
2023-12-29 06:58:48 -0600

    w-auto

--- app/components/feed/postsContainer/PostImage.tsx
+++ app/components/feed/postsContainer/PostImage.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const PostImage = ({ postImage } : { postImage: string }) => {
     return (
       <div className="mt-2">
-        <Image src={postImage} width={510} height={510} alt="" className="rounded-2xl max-h-[510px]" />
+        <Image src={postImage} width={510} height={510} alt="" className="rounded-2xl max-h-[510px] w-auto" />
       </div>
     );
 }
