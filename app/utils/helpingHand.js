ea01535d5dbc29bd17239a043e4536472f5540bb
NBojan <63514753+NBojan@users.noreply.github.com>
2024-08-21 07:01:01 -0500

    url

--- app/utils/helpingHand.js
+++ app/utils/helpingHand.js
@@ -2,12 +2,12 @@ export const logo = 'https://firebasestorage.googleapis.com/v0/b/twitter-clone-b
 export const maleImg = 'https://firebasestorage.googleapis.com/v0/b/twitter-clone-bf6ea.appspot.com/o/appImages%2Fmale.png?alt=media&token=5323885a-15d7-4ac3-a738-76bd30483154';
 export const femaleImg = 'https://firebasestorage.googleapis.com/v0/b/twitter-clone-bf6ea.appspot.com/o/appImages%2Ffemale.png?alt=media&token=d86cceb7-508a-4855-9f03-6b66644cd303';
 
-// export const loginUrl = 'https://nb-twitter.vercel.app/api/auth/login';
+export const loginUrl = 'https://nb-twitter.vercel.app/api/auth/login';
 export const registerUrl = 'https://nb-twitter.vercel.app/api/auth/register';
-// export const tokenLoginUrl = 'https://nb-twitter.vercel.app/api/auth/tokenLogin';
-export const tokenLoginUrl = 'http://localhost:3000/api/auth/api/auth/tokenLogin';
+export const tokenLoginUrl = 'https://nb-twitter.vercel.app/api/auth/tokenLogin';
+// export const tokenLoginUrl = 'http://localhost:3000/api/auth/api/auth/tokenLogin';
 
-export const loginUrl = 'http://localhost:3000/api/auth/login';
+// export const loginUrl = 'http://localhost:3000/api/auth/login';
 
 
 export const newsApi = "https://saurav.tech/NewsAPI/top-headlines/category/general/us.json";
