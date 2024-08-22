cdfa20482ff29bcd20e23d6aed123dab748b9053
NBojan <63514753+NBojan@users.noreply.github.com>
2024-08-22 05:46:34 -0500

    slector

--- app/components/profile/User.tsx
+++ app/components/profile/User.tsx
@@ -7,9 +7,9 @@ const User = ({ userData, userPosts } : { userData: userElement, userPosts: post
             <Image src={userData.userImg} width={96} height={96} alt="UserImg" className="rounded-full w-[96px] h-[96px] border-2 theme-grayBorder-200-700" />
 
             <div>
-                <p className="font-semibold tracking-wide text-lg dark:text-twitterTextDarkmode">{userData.firstName} {userData.lastName}</p>
-                <p className="font-semibold tracking-wide text-lg dark:text-twitterTextDarkmode">@{userData.username}</p>
-                <p className="theme-gray500-gray400">{userPosts.length} {userPosts.length === 1 ? "tweet" : "tweets"}</p>
+                <p className="font-semibold tracking-wide text-lg dark:text-twitterTextDarkmode profile-name">{userData.firstName} {userData.lastName}</p>
+                <p className="font-semibold tracking-wide text-lg dark:text-twitterTextDarkmode profile-username">@{userData.username}</p>
+                <p className="theme-gray500-gray400 profile-postLength">{userPosts.length} {userPosts.length === 1 ? "tweet" : "tweets"}</p>
             </div>
         </div>
     );
